a0ccbdd5a5a0a58986f67e5940459a83785075ac
bjralexandru <bjalexandru@gmail.com>
2023-01-01 17:04:49 +0200

    initial commit

---
+++ src/test/java/com/bjr/UserRegistration/UserRepositoryTests.java
@@ -0,0 +1,40 @@
+package com.bjr.UserRegistration;
+
+
+import com.bjr.UserRegistration.model.User;
+import com.bjr.UserRegistration.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("testemail@gmail.com");
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setPassword("123456");
+
+        User savedUser = repo.save(user);
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+    }
+}
